5e840ef20831157eb0f6641bda549c8ed14de2cf
Joseph Christopher Jacob <36853161+X10TION@users.noreply.github.com>
2022-02-13 00:30:48 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const express = require ('express')
+const express = require ('express');
+const path = require('path')
 const mysql = require ("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config({path:'./.env'});
 
 const app = express();
 const db =mysql.createConnection( {
-    host: 'localhost', 
-    user : 'root',
-    password:  '',
-    database : 'wallet'
+    host: process.env.DATABASE_HOST, 
+    user : process.env.DATABASE_USER,
+    password:  process.env.DATABASE_PASSWORD,
+    database : process.env.DATABASE,
 
 });
+
+const publicDirectory = path.join(__dirname,'./public');
+app.use(express.static(publicDirectory));
+
+app.set('view engine', 'hbs');
 
 db.connect ((error) => {
     if(error) {
@@ -20,7 +29,8 @@ db.connect ((error) => {
 
 })
 app.get("/", (req,res) =>  {
-    res.send(  " <h1>home page </h1> ")
+    // res.send(  " <h1>home page </h1> ")
+    res.render("index")
 })
 
 app.listen(5000, () => { 
